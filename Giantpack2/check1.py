fa5b53a68a0f1eae5903c6646ce1dec7cdd42f1c
gittyhub <junkindabox@gmail.com>
2015-01-19 20:27:59 -0500

    made changes to giants, need to run when tickets available

--- Giantpack2/check1.py
+++ Giantpack2/check1.py
@@ -7,7 +7,7 @@ if __name__=='__main__':
 
   date1 = sys.argv[1].split('/')
   site = 'http://sfbay.craigslist.org/search/sss?sort=rel&query=giants%20+'+date1[0]+'%2F'+date1[1]+'&minAsk=&maxAsk=&sort=date'
-  i = int(check.x(5))
+  i = int(check.x(5))# this line is just to check so you should see 15 when you run it
   print(i)
   siter = Modules_List.get_AllLinks1(site, sys.argv[1])
   print(siter)
